02a1a806ac7162f21cc59972de3621d6c0b243cb
sonypiay <pyscho30@gmail.com>
2024-10-25 02:23:07 +0700

    remove getter setter RegisterUserRequest class

--- src/main/java/programmerzamannow/restful/model/RegisterUserRequest.java
+++ src/main/java/programmerzamannow/restful/model/RegisterUserRequest.java
@@ -24,37 +24,4 @@ public class RegisterUserRequest {
     @NotBlank
     @Size(max = 100)
     private String name;
-
-    public RegisterUserRequest(String username, String password, String name) {
-        this.username = username;
-        this.password = password;
-        this.name = name;
-    }
-
-    public RegisterUserRequest() {
-    }
-
-    public @NotBlank @Size(max = 100) String getUsername() {
-        return username;
-    }
-
-    public void setUsername(@NotBlank @Size(max = 100) String username) {
-        this.username = username;
-    }
-
-    public @NotBlank @Size(max = 100) String getPassword() {
-        return password;
-    }
-
-    public void setPassword(@NotBlank @Size(max = 100) String password) {
-        this.password = password;
-    }
-
-    public @NotBlank @Size(max = 100) String getName() {
-        return name;
-    }
-
-    public void setName(@NotBlank @Size(max = 100) String name) {
-        this.name = name;
-    }
 }
