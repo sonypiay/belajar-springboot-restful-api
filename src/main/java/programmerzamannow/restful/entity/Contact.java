1f3b0a5752541526e387e1d205afcc0e4d3ee6f5
sonypiay <pyscho30@gmail.com>
2024-10-25 00:43:57 +0700

    create entity

---
+++ src/main/java/programmerzamannow/restful/entity/Contact.java
@@ -0,0 +1,36 @@
+package programmerzamannow.restful.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    private String id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "username", referencedColumnName = "username")
+    private User user;
+
+    @OneToMany(mappedBy = "contact")
+    private List<Address> addressList;
+}
